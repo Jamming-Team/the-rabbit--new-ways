4e4b5826aae0e5baa888ccf868fb3780971b352c
xidriel <stormingisle@proton.me>
2025-04-26 17:36:20 +0300

    Added shaking to active head

--- Assets/Rabbit/Code/Gameplay/DialogueHeadController.cs
+++ Assets/Rabbit/Code/Gameplay/DialogueHeadController.cs
@@ -9,15 +9,16 @@ namespace Rabbit {
         [SerializeField] Image _headImage;
         
         Sequence _sequence;
+        Sequence _sequence2;
         Vector3 _defaultScale = Vector3.one;
         Vector3 _increasedScale = new Vector3(1.5f, 1.5f, 1f);
         bool _shouldIncrease;
         Vector3 _rotationVector = new Vector3(0f, 0f, 7);
 
         public void Awake() {
-            Sequence.Create()
-                .Chain(Tween.ShakeLocalRotation(_headImage.transform, strength: new Vector3(0, 0, 15), duration: 1,
-                    frequency: 10));
+            // Sequence.Create()
+            //     .Chain(Tween.ShakeLocalRotation(_headImage.transform, strength: new Vector3(0, 0, 4), duration: 10000,
+            //         frequency: 6));
         }
 
 
@@ -29,13 +30,21 @@ namespace Rabbit {
                 return;
             
             _shouldIncrease = shouldIncrease;
-            
-            if (shouldIncrease)
+
+            if (shouldIncrease) {
                 _sequence = Sequence.Create()
-                    .Chain(Tween.Custom(_defaultScale, _increasedScale, 1f, newVal => _headImage.transform.localScale = newVal));
-            else
+                    .Chain(Tween.Custom(_defaultScale, _increasedScale, 1f,
+                        newVal => _headImage.transform.localScale = newVal));
+                _sequence2 = Sequence.Create()
+                    .Chain(Tween.ShakeLocalRotation(_headImage.transform, strength: new Vector3(0, 0, 4), duration: 10000,
+                        frequency: 6));
+            }
+            else {
                 _sequence = Sequence.Create()
-                    .Chain(Tween.Custom(_increasedScale, _defaultScale, 1f, newVal => _headImage.transform.localScale = newVal));
+                    .Chain(Tween.Custom(_increasedScale, _defaultScale, 1f,
+                        newVal => _headImage.transform.localScale = newVal));
+                _sequence2.Complete();
+            }
         }
     }
 }
