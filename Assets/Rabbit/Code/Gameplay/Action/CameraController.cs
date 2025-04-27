a0be759b5f9cf98583bfac964b5275287c8645ed
Alexandr Kochurov <b2b4ly@gmail.com>
2025-04-27 20:27:21 +0200

    feat: add camera rotarion

--- Assets/Rabbit/Code/Gameplay/Action/CameraController.cs
+++ Assets/Rabbit/Code/Gameplay/Action/CameraController.cs
@@ -6,6 +6,9 @@ namespace Rabbit {
         [SerializeField] Camera _camera;
         [SerializeField] LayerMask _layerInteractive;
 
+        private float rotationSpeed = 70f; 
+        private float maxRotationAngle = 80f;
+        private float currentRotation = 0f;
 
         void Awake() {
             GameEvents.Gameplay.OnGameplayUpdate += OnGameplayUpdate;
@@ -15,8 +18,33 @@ namespace Rabbit {
             GameEvents.Gameplay.OnGameplayUpdate -= OnGameplayUpdate;
         }
 
-        void OnGameplayUpdate(float obj) {
+        void OnGameplayUpdate(float deltaTime) {
+            RotateCamera(deltaTime);
             DetectClick();
+        }
+
+        private void RotateCamera(float deltaTime) {
+            float rotationInput = 0f;
+
+            
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                rotationInput -= 1f;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                rotationInput += 1f;
+            }
+            else return;
+
+            
+            float rotationAmount = rotationInput * rotationSpeed * deltaTime;
+            float targetRotation = Mathf.Clamp(currentRotation + rotationAmount, -maxRotationAngle, maxRotationAngle);
+
+            float realRotationAmount = targetRotation - currentRotation;
+            transform.Rotate(0f, realRotationAmount, 0f);
+
+            currentRotation = targetRotation;
         }
 
         private void DetectClick() 
